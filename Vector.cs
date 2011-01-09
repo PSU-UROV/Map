f32791d696096510332c11b60e3f79f908ce59da
Lewis Coates <lewisc@pdx.edu>
2011-01-09 04:49:05 -0800

    Initial revision of the map server, currently not quite complete but it does compile, specifically no serialization is enabled and the bool array isn't done. I

---
+++ Vector.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapData
+{
+    /// <summary>
+    /// Point is an immutable x/y value pair, initialized by the constructor
+    /// This struct is designed to be interoperable simply(blittable)
+    /// </summary>
+    public struct Point
+    {
+           private readonly float _xval;
+           private readonly float _yval;
+           /// <summary>
+           /// builds an immutable point
+           /// </summary>
+           /// <param name="xval">assigns the x value</param>
+           /// <param name="yval">assigns the yvalue</param>
+           public Point(float xval, float yval)
+           {
+               this._xval = xval;
+               this._yval = yval;
+
+           }
+
+
+            /// <summary>
+            /// returns the xValue
+            /// </summary>
+           public float Xval
+           {
+               get
+               {
+                   return _xval;
+               }
+           }
+
+            /// <summary>
+            /// returns the yvalue
+            /// </summary>
+           public float Yval
+           {
+               get
+               {
+                   return _yval;
+               }
+           }
+    }
+
+    /// <summary>
+    /// a line is an immutable struct of 2 points, it has one function
+    /// that tests if another line intersects this line
+    /// </summary>
+    public struct Line
+    {
+        private readonly Point _pointa;
+        private readonly Point _pointb;
+
+        /// <summary>
+        /// builds an immutable line
+        /// </summary>
+        /// <param name="pointa">assigns point A immutably</param>
+        /// <param name="pointb">assigns B immutably</param>
+        public Line(Point pointa, Point pointb)
+        {
+            _pointa = pointa;
+            _pointb = pointb;
+        }
+    
+
+        /// <summary>
+        /// returns point a
+        /// </summary>
+        public Point PointA
+        {
+            get
+            {
+                return _pointa;
+            }
+        }
+
+        /// <summary>
+        /// returns point b
+        /// </summary>
+        public Point PointB
+        {
+            get
+            {
+                return _pointb;
+            }
+        }
+
+        /// <summary>
+        /// Tests whether or not another line intersects with this one
+        /// designed for testing paths composed of vectors
+        /// </summary>
+        /// <param name="test">the line to test against</param>
+        /// <returns>true if it does intersect, false if it doesn't. Note this may be inverted logic to
+        /// what is desired since most likely you are looking for "does not cross", i.e. "doesn cross == false"
+        /// </returns>
+        ///<remarks> code partially cribbed from http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/Helpers.cs
+        ///http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+        /// second one is the math
+        ///</remarks>
+        public bool DoesIntersect(Line test)
+        {
+            float denominator = (test.PointA.Yval - test.PointB.Yval) * (this.PointA.Xval - this.PointB.Xval)
+                        - (test.PointA.Xval - test.PointA.Xval) * (this.PointA.Yval - this.PointB.Yval);
+
+
+            float numerator_a = (test.PointA.Xval - test.PointB.Xval) * (this.PointB.Yval - test.PointB.Yval)
+                            - (test.PointA.Yval - test.PointB.Yval) * (this.PointB.Xval - test.PointA.Xval);
+
+            float numerator_b = (this.PointA.Xval - this.PointB.Xval) * (this.PointB.Yval - test.PointB.Yval)
+                            - (this.PointA.Yval - this.PointB.Yval) * (this.PointB.Xval - test.PointA.Xval);
+
+
+            //supposedly this solves if the two lines are parallell and touch(need to run the numbers myself)
+            //I suspect there is a bug if 2 lines are paralell and overlapping for part(will test and eventually correct)
+            if(denominator == 0)
+                return false;
+
+            double slopea = numerator_a / denominator;
+            double slopeb = numerator_b / denominator;
+
+            //if slopea is between 0 and 1, and slopeb is between 0 and one, then the two lines intersect
+            if (slopea >= 0 && slopea <= 1 && slopeb >= 0 && slopeb <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+    }
+}
---
+++ VectorList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapData
+{
+    /// <summary>
+    /// A collection of vectors that surmise the surfaces on a map.
+    /// you can get this either as a list of vectors or as a double array of bools, true means passable, false means impassable
+    /// This will eventually be serializable to JSON
+    /// </summary>
+    public class Map
+    {
+        private readonly List<Line> _surfaces = new List<Line>();
+        /// <summary>
+        /// empty constructor, does nothing
+        /// </summary>
+        Map()
+        {
+        }
+
+        /// <summary>
+        /// returns the Surfaces as a list
+        /// </summary>
+        public List<Line> Surfaces
+        {
+            get
+            {
+                return _surfaces;
+            }
+        }
+
+        /// <summary>
+        /// adds a specific surface along the given lines
+        /// </summary>
+        /// <param name="from">a point the line starts at</param>
+        /// <param name="to">a point the line ends at</param>
+        public void AddNewLine(Point from, Point to)
+        {
+            _surfaces.Add(new Line(new Point(from.Xval, from.Yval), new Point(to.Xval, to.Yval )));
+        }
+
+        /// <summary>
+        /// adds a new line with coordinates between 0 and 100 completely randomly
+        /// </summary>
+        public void AddNewRandomLine()
+        {
+            Random gen = new Random();
+            Point from = new Point(gen.Next(100), gen.Next(100));
+            Point to = new Point(gen.Next(100), gen.Next(100));
+
+            this.AddNewLine(from, to);
+        }
+
+        /// <summary>
+        /// returns the map as an array of boolean values from 0 to 100, 0 to 100
+        /// if the value is false, the terrain is impassable, if the value is true, the terrain is
+        /// passable
+        /// </summary>
+        public bool[,] MapData
+        {
+            get
+            {
+                bool[,] toret = new bool[100,100];
+                //negate out all the surface area as impassable
+                //get the initial point, negate it, get the slope,
+                //negate every value along the slope until the end.
+                foreach (Line negate in Surfaces)
+                {
+                    float slope = (negate.PointA.Yval - negate.PointB.Yval) / (negate.PointA.Xval - negate.PointB.Xval);
+
+                    toret[(int)negate.PointA.Xval, (int)negate.PointB.Yval] = false;
+
+                }
+                return toret;
+                     
+            }
+        }
+    }
+}
