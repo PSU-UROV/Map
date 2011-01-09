0ba791df91b392cb27e358d30ed0b4b56afbc15e
Lewis Coates <lewisc@pdx.edu>
2011-01-09 13:35:27 -0800

    added main loop

---
+++ MapData/ServerMain/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerMain
+{
+    class Loop
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+
+            
+        }
+    }
+}
